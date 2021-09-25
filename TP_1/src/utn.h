891db6545e407c5703f825306fd2f070db750051
tamyt <tamitugues@gmail.com>
2021-09-25 20:12:57 -0300

    doxygen

--- TP_1/src/utn.h
+++ TP_1/src/utn.h
@@ -7,44 +7,43 @@
 
 #ifndef UTN_H_
 #define UTN_H_
-/**
- * \brief suma operando1 y operando2
- * \param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
- * \param int operando1,primer operando.
- * \param int operando2,segundo operando.
- * \return (-1) Error / (0) Ok
- */
+/// @fn int sumar(int, int, int*)
+/// @brief suma operando1 y operando2
+/// @param int operando1,primer operando.
+/// @param int operando2,segundo operando.
+/// @param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
+/// @return (-1) Error / (0) Ok
 int sumar(int operando1, int operando2, int* resultado);
-/**
- * \brief resta operando1 y operando2
- * \param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
- * \param int operando1,primer operando.
- * \param int operando2,segundo operando.
- * \return (-1) Error / (0) Ok
- */
+
+/// @fn int restar(int, int, int*)
+/// @brief resta operando1 y operando2
+/// @param int operando1,primer operando.
+/// @param int operando2,segundo operando.
+/// @param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
+/// @return (-1) Error / (0) Ok
 int restar(int operando1, int operando2, int* resultado);
-/**
- * \brief multiplica operando1 por operando2
- * \param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
- * \param int operando1,primer operando.
- * \param int operando2,segundo operando.
- * \return (-1) Error / (0) Ok
- */
+
+/// @fn int multiplicar(int, int, int*)
+/// @brief multiplica operando1 por operando2
+/// @param int operando1, primer operando.
+/// @param int operando2, segundo operando
+/// @param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
+/// @return (-1) Error / (0) Ok
 int multiplicar(int operando1, int operando2, int* resultado);
-/**
- * \brief divide operando1 con operando2
- * \param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
- * \param int operando1,primer operando.
- * \param int operando2,segundo operando.
- * \return (-1) Error / (0) Ok
- */
+
+/// @fn int dividir(int, int, float*)
+/// @brief divide operando1 con operando2
+/// @param int operando1,primer operando.
+/// @param int operando2,segundo operando.
+/// @param float* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
+/// @return (-1) Error / (0) Ok
 int dividir(int operando1, int operando2, float* resultado);
-/**
- * \brief saca el factorial de operando
- * \param int* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
- * \param int operando,operando del que sacamos el factorial.
- * \return (-1) Error / (0) Ok
- */
+
+/// @fn int sacarFactorial(int, int*)
+/// @brief saca el factorial de operando
+/// @param int operando,operando del que sacamos el factorial.
+/// @param nt* resultado, puntero al espacio de memoria donde se dejara  el valor obtenido.
+/// @return (-1) Error / (0) Ok
 int sacarFactorial(int operando, int* resultado);
 
 
